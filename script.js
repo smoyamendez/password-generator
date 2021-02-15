ddbc4961426a3e74a9ac8b08e2a3a1811d55cfd1
Sommer Moya-Mendez <smoyamendez@gmail.com>
2021-02-15 18:24:56 -0500

    password generating and changed concat to push to form array of arrays

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ generateBtn.addEventListener("click", writePassword);
 
 
 //  Generate password after all inputs from user
-
+function getRandom (array) {
+  var randomIndex = Math.floor(Math.random() * array.length);
+  var randomCharacter = array[randomIndex];
+  return randomCharacter;
+  }
 function generatePassword() {
   
   // All character variables
@@ -22,56 +26,59 @@ function generatePassword() {
   var uppercaseCheckbox = document.querySelector("#wantsUppercase");
   var lowercaseCheckbox = document.querySelector("#wantsLowercase");
   var specialCheckbox = document.querySelector("#wantsSpecialCharacters");
-
-  // Blank variable to collect all of user's inputs
-
-  var userInput = "";
-
-  // Function for random index of array
-  function getRandom (array) {
-    var randomIndex = Math.floor(Math.random() * array.length);
-    var randomCharacter = array[randomIndex];
-    return randomCharacter;
-  }
-
+  
   // variable for the length of password
   var passwordLength = document.querySelector("#lengthInput").value;
-  
+
+  // Blank variable to collect all of user's inputs
+  var userInput = [];
+
   console.log(passwordLength);
   
   // Adding alerts if user input number < 8 or > 128
 
   if (passwordLength < 8) {
     alert("Password must be at least 8 characters in length.");
+    return "";
   }
 
   if (passwordLength > 128) {
     alert("Password must not be more than 128 characters in length.");
+    return "";
   }
 
+  // Function for random index of array
+
+  
   // Adds arrays of characters based on what is checked by user to the userInput variable
 
   if (numberCheckbox.checked) {
-    userInput = userInput.concat(numbers);
+    userInput.push(numbers);
   }
   if (uppercaseCheckbox.checked) {
-    userInput = userInput.concat(upperLetters);
+    userInput.push(upperLetters);
   }
   if (lowercaseCheckbox.checked) {
-    userInput = userInput.concat(lowerLetters);
+    userInput.push(lowerLetters);
   }
   if (specialCheckbox.checked) {
-    userInput = userInput.concat(specialCharacters);
+    userInput.push(specialCharacters);
   }
   
   console.log(userInput);
   
-  var finalPassword = function {
-    getRandom (userInput) * passwordLength;
-  }
+  var finalPassword = [];
+ 
+  for (var i = 0; i < passwordLength; i++) {
+    var arr = userInput[Math.floor(Math.random()*userInput.length)];
+    var char = getRandom(arr);
+    finalPassword.push(char);
 
- return finalPassword;
+  } 
+  return finalPassword.join('');
+  
 }
+
 
 // Write password to the #password input
 function writePassword() {
